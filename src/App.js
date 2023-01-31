daee43843d1afa1416be31375be8cc89a7860888
apk09 <ashishkhadka09@gmail.com>
2023-01-31 12:49:41 +0530

    Comment kata app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+// import KataApp from './components/kata/kata-app';
 import KumiteApp from './components/kumite/kumite-app';
 
 function App() {
   return (
     <div className="App">
       <KumiteApp />
+      {/* <KataApp /> */}
     </div>
   );
   
